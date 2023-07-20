bf190dacb87d435827d849a07613949c9728b6f1
Pini <pini.tzr@gmail.com>
2023-07-20 20:24:01 +0200

    Changes made on 20/07/2023 at 20:24:01,66 from PC: DESKTOP-F6R5C8I

--- push.bat
+++ push.bat
@@ -1,6 +1,10 @@
 @echo off
 git pull
 git add .
-set "commit_message=Changes made on %date% at %time%"
+set "commit_message=Changes made on %date% at %time% from PC: %computername%"
 git commit -m "%commit_message%"
 git push origin
+
+
+
+
