5cc4a33665a8bfb2dc61dcae4999afb3fb324adc
Pini <pini.tzr@gmail.com>
2023-08-04 09:19:06 +0200

    Changes made on 04/08/2023 at  9:19:06,23 from PC Home

--- React/Project Structure/proto_project/src/App.js
+++ React/Project Structure/proto_project/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
 import  {AllRoutes} from "../src/routes/AllRoutes"
 function App() {
   return (
-      <BrowserRouter>
            <AllRoutes />
-      </BrowserRouter>
 
   );
 }
